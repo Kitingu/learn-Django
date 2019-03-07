9a1eb3b842df7a8cfdec90818436044cc8b5aa16
benlegendj <benlegendj@gmail.com>
2019-03-07 08:43:22 +0300

    add and render html templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'corey ms',
+        'title': "blog post 1",
+        'content': 'asdfg fgh',
+        'date_posted': 'August 27 2018'
+    },
+    {
+        'author': 'bendeh',
+        'title': "blog post 2",
+        'content': 'allah wakubar',
+        'date_posted': 'August 27 2017'
+    }
+]
 
 
 def home(request):  # must take the response argument
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> About us </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
